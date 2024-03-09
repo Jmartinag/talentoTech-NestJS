d7663c4df21ea05255c5556962905b100064b471
Jhon Martin <martinjhon@gmail.com>
2024-03-08 21:00:07 -0500

    add login

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HousesModule } from './houses/houses.module';
 import { MessagesModule } from './messages/messages.module';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
 import * as dotenv from 'dotenv';
 
 dotenv.config(); //Cargamos las variables de configuracion
